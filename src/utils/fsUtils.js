05cff89680c7375bf058fa902000a813ff1b59a0
andersonlouzada <anderson.s.louzada@gmail.com>
2023-08-15 11:01:50 -0300

    cria funcões de leitura e escrita auxiliares

---
+++ src/utils/fsUtils.js
@@ -0,0 +1,31 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+const TALKER_DATA_PATH = '.././talker.json';
+
+async function readTalkers() {
+  try {
+    const data = await fs.readFile(path.resolve(__dirname, TALKER_DATA_PATH));
+    const talkers = JSON.parse(data);
+
+    console.log(talkers);
+  } catch (error) {
+    console.error(`Erro de leitura: ${error}`);
+  }
+}
+
+async function writeTalkers(newTalker) {
+  try {
+    const oldTalkers = await readTalkers();
+    const newTalkers = JSON.stringify([...oldTalkers, newTalker]);
+
+    await fs.writeFile(path.resolve(__dirname, TALKER_DATA_PATH), newTalkers);
+  } catch (error) {
+    console.error(`Erro de leitura: ${error}`);
+  }
+}
+
+module.exports = {
+  readTalkers,
+  writeTalkers,
+};
